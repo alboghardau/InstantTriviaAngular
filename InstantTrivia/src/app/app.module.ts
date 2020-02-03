caeb3c27e034592a81f733a0c32280e6cea43aca
albog <alboghardau@gmail.com>
2020-02-04 00:54:01 +0200

    login component

--- InstantTrivia/src/app/app.module.ts
+++ InstantTrivia/src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
